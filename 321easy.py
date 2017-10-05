ed612885f69cadc6fc66ca795644daf4e669b025
Danny V <danny@dannyvacar.ca>
2017-10-05 09:11:44 -0400

    Fixed extra space when minutes were "00"

--- 321easy.py
+++ 321easy.py
@@ -1,12 +1,27 @@
 import inflect, fileinput
-
+'''
+Sample input:
+00:00
+01:30
+12:05
+14:01
+20:29
+21:00
+Sample output:
+It's twelve am
+It's one thirty am
+It's twelve oh five pm
+It's two oh one pm
+It's eight twenty nine pm
+It's nine pm
+'''
 p = inflect.engine()
 hr_corner_cases = {'00':'twelve'}
 min_corner_cases = {'00':''}
 ampm = 'am'
 
 for t in fileinput.input():
-    hr, mn = (t.strip()).split(":") 
+    hr, mn = (t.strip()).split(":")
 
     if hr in hr_corner_cases:
         shr = hr_corner_cases[hr]
@@ -22,6 +37,6 @@ for t in fileinput.input():
         smn = p.number_to_words(mn)
         if int(mn) < 10:
             smn = 'oh ' + smn
+        smn = ' ' + smn
 
-    #TODO: there's an extra space when minutes are 00
-    print("It's {}{}{}".format(shr, ' '+smn, ' '+ampm))
+    print("It's {}{}{}".format(shr, smn, ' '+ampm))
