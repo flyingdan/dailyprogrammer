684f3cb6d83478ddeb125805d69b81a773e22196
flyingdan <1051205+flyingdan@users.noreply.github.com>
2017-10-25 16:10:07 -0400

    new problem

---
+++ 337easy.py
@@ -0,0 +1,6 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/784fgr/20171023_challenge_337_easy_minimize_maximize/
+
+# Area of circle = (pi)r^2
+# Circumference of circle = 2(pi)r
+# Area of sector covering x of circumference = rx/2
+#   x - angle subtended by arc
